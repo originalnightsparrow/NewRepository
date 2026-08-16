40e63c613edbcaf40c2e4eb1d3b4c2f3864f5b62
Daniel Farthing <griffon1689@gmail.com>
2026-08-16 18:03:44 -0400

    Add welcome message to hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,9 @@
 # A simple Python program to print a message
 
 print ("Hello, World!")
+
+# Daniel Farthing
+# 08/16/2026 a little later
+# A simple Python program to print a message
+
+print ("Welcome to GitHub!")
